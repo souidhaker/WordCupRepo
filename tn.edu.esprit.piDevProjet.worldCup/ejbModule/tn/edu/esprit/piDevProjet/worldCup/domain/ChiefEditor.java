eb7e48900a83e858a3863f766e1fef7dcedebae0
Houssem <Houssem@172.16.184.214>
2013-10-07 21:57:00 +0100

    ChefEditor
    Signed-off-by: Houssem <Houssem@172.16.184.214>

---
+++ tn.edu.esprit.piDevProjet.worldCup/ejbModule/tn/edu/esprit/piDevProjet/worldCup/domain/ChiefEditor.java
@@ -0,0 +1,47 @@
+package tn.edu.esprit.piDevProjet.worldCup.domain;
+
+import java.io.Serializable;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: ChiefEditor
+ *
+ */
+@Entity
+
+public class ChiefEditor implements Serializable {
+
+	   
+	
+	private String login;
+	private String password;
+	private static final long serialVersionUID = 1L;
+
+	public ChiefEditor() {
+		super();
+	}   
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}   
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public ChiefEditor(String login, String password) {
+		super();
+		this.login = login;
+		this.password = password;
+	}
+	
+   
+}
