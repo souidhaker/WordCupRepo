19a3718b2c42d4f22276767487104288eb403125
Houssem <Houssem@172.16.184.214>
2013-10-07 21:42:40 +0100

    Match entity
    Signed-off-by: Houssem <Houssem@172.16.184.214>

---
+++ tn.edu.esprit.piDevProjet.worldCup/ejbModule/tn/edu/esprit/piDevProjet/worldCup/domain/Match.java
@@ -0,0 +1,76 @@
+package tn.edu.esprit.piDevProjet.worldCup.domain;
+
+import java.io.Serializable;
+import java.lang.String;
+import java.sql.Date;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Match
+ *
+ */
+@Entity
+
+public class Match implements Serializable {
+
+	   
+	
+	private int matchId;
+	private String matchReferee;
+	private Date matchDate;
+	private String matchCategory;
+	private String matchStadium;
+	private static final long serialVersionUID = 1L;
+
+	public Match() {
+		super();
+	}   
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public int getMatchId() {
+		return this.matchId;
+	}
+
+	public void setMatchId(int matchId) {
+		this.matchId = matchId;
+	}   
+	public String getMatchReferee() {
+		return this.matchReferee;
+	}
+
+	public void setMatchReferee(String matchReferee) {
+		this.matchReferee = matchReferee;
+	}   
+	public Date getMatchDate() {
+		return this.matchDate;
+	}
+
+	public void setMatchDate(Date matchDate) {
+		this.matchDate = matchDate;
+	}   
+	public String getMatchCategory() {
+		return this.matchCategory;
+	}
+
+	public void setMatchCategory(String matchCategory) {
+		this.matchCategory = matchCategory;
+	}   
+	public String getMatchStadium() {
+		return this.matchStadium;
+	}
+
+	public void setMatchStadium(String matchStadium) {
+		this.matchStadium = matchStadium;
+	}
+	public Match(int matchId, String matchReferee, Date matchDate,
+			String matchCategory, String matchStadium) {
+		super();
+		this.matchId = matchId;
+		this.matchReferee = matchReferee;
+		this.matchDate = matchDate;
+		this.matchCategory = matchCategory;
+		this.matchStadium = matchStadium;
+	}
+	
+   
+}
